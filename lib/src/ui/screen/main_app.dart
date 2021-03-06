4166abc7e72064cf1fe0d72dbac8975eea29b4a2
Dreamwalker <aristojeff@gmail.com>
2021-03-06 22:04:44 +0900

    Update main_app.dart

--- lib/src/ui/screen/main_app.dart
+++ lib/src/ui/screen/main_app.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'home_page.dart';
 
-
 class Covid19VaccineApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        initialRoute: '/',
-      routes: {
-          "/" : (context) => HomePage()
-      },
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      initialRoute: '/',
+      routes: {"/": (context) => HomePage()},
     );
   }
 }
