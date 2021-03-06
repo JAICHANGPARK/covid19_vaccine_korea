12225bd692909c7bef109442d0992f0470383fd2
Dreamwalker <aristojeff@gmail.com>
2021-03-06 23:23:51 +0900

    Update setting_page.dart

--- lib/src/ui/screen/setting/setting_page.dart
+++ lib/src/ui/screen/setting/setting_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SettingPage extends StatefulWidget {
   @override
   _SettingPageState createState() => _SettingPageState();
@@ -10,16 +9,52 @@ class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text("설정",style: TextStyle(
-              fontSize: 24
-            ),),
-            ListTile(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: Icon(Icons.arrow_back)),
+                  Text(
+                    "설정",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                child: Text("앱 정보"),
+              ),
+              PhysicalModel(
+                color: Colors.grey,
+                child: Container(
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text("버전 정보"),
+                      ),
+                      Divider(height: 8,),
+                      ListTile(
+                        onTap: (){
+                          showLicensePage(context: context);
+                        },
+                        title: Text("오픈소스 라이선스"),
+                      )
+                    ],
+                  ),
+                ),
+              ),
 
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
