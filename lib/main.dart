5468d06cdfc63f74002521351c19c8f6bdf8e648
Dreamwalker <aristojeff@gmail.com>
2021-03-06 22:21:02 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:fimber/fimber.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'src/ui/screen/main_app.dart';
 
 void main() {
+  if(kDebugMode){
+    Fimber.plantTree(DebugTree());
+    Fimber.d('debug mode');
+  }
   runApp(Covid19VaccineApp());
 }
