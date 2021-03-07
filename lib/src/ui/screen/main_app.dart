7e20162b166d57b98168ad1417aa872c3b18654d
Dreamwalker <aristojeff@gmail.com>
2021-03-07 14:50:42 +0900

    Update main_app.dart

--- lib/src/ui/screen/main_app.dart
+++ lib/src/ui/screen/main_app.dart
@@ -1,7 +1,12 @@
+
 import 'package:covid_19_vaccine_korea/src/ui/screen/setting/setting_page.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
+
+FirebaseAnalytics analytics = FirebaseAnalytics();
 
 class Covid19VaccineApp extends StatelessWidget {
   @override
@@ -9,6 +14,9 @@ class Covid19VaccineApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
       initialRoute: '/',
       routes: {
         "/": (context) => HomePage(),
