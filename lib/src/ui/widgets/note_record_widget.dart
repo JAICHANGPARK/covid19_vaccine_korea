ffaf4237c120de04adbfc8162546ffb8d2a8bce8
Dreamwalker <aristojeff@gmail.com>
2021-03-14 00:38:42 +0900

    Update note_record_widget.dart

--- lib/src/ui/widgets/note_record_widget.dart
+++ lib/src/ui/widgets/note_record_widget.dart
@@ -15,18 +15,20 @@ class _NoteRecordWidgetState extends State<NoteRecordWidget> {
   Box<Note>? _noteBox;
   List<Note>? noteItems = [];
   bool isLoading = false;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     Fimber.d(">>> initState");
-    Future.delayed(Duration(seconds: 2), (){
+    Future.delayed(Duration(seconds: 2), () {
       isLoading = true;
       setState(() {
-        noteItems =  getIt.get<AppDBProvider>().noteBox?.values.toList();
+        noteItems = getIt.get<AppDBProvider>().noteBox?.values.toList();
       });
     });
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
@@ -36,22 +38,24 @@ class _NoteRecordWidgetState extends State<NoteRecordWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return noteItems!.length > 0  ? ListView.builder(
-      itemCount: noteItems!.length,
-      itemBuilder: (context, index) {
-        return Text("${noteItems![index].created.toString()}: ${noteItems![index].symptom}");
-      },
-    ) : Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-          isLoading ? Container() : Center(child: Column(
+    return noteItems!.length > 0
+        ? ListView.builder(
+            itemCount: noteItems!.length,
+            itemBuilder: (context, index) {
+              return Text("${noteItems![index].created.toString()}: ${noteItems![index].symptom}");
+            },
+          )
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              CircularProgressIndicator(),
-              Text("불러오는 중.")
+              isLoading
+                  ? Container()
+                  : Center(
+                      child: Column(
+                      children: [CircularProgressIndicator(), Text("불러오는 중.")],
+                    ))
             ],
-          ))
-      ],
-    );
+          );
   }
 }
