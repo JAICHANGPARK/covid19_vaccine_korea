62d033bbe2f14689d699d4935101fc7c3d9b5e2b
Dreamwalker <aristojeff@gmail.com>
2021-03-14 00:36:26 +0900

    Update main_app.dart

--- lib/src/ui/screen/main_app.dart
+++ lib/src/ui/screen/main_app.dart
@@ -1,8 +1,10 @@
 
+import 'package:covid_19_vaccine_korea/src/db/db.dart';
 import 'package:covid_19_vaccine_korea/src/ui/screen/setting/setting_page.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'home_page.dart';
 
@@ -11,17 +13,35 @@ FirebaseAnalytics analytics = FirebaseAnalytics();
 class Covid19VaccineApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      navigatorObservers: [
-        FirebaseAnalyticsObserver(analytics: analytics),
-      ],
-      initialRoute: '/',
-      routes: {
-        "/": (context) => HomePage(),
-        "/setting": (context) => SettingPage(),
-      },
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (context) => AppDBProvider()),
+    ],
+
+
+     child:MaterialApp(
+       theme: ThemeData.light(),
+       darkTheme: ThemeData.dark(),
+       navigatorObservers: [
+         FirebaseAnalyticsObserver(analytics: analytics),
+       ],
+       initialRoute: '/',
+       routes: {
+         "/": (context) => HomePage(),
+         "/setting": (context) => SettingPage(),
+       },
+     ) ,
     );
+    // return MaterialApp(
+    //   theme: ThemeData.light(),
+    //   darkTheme: ThemeData.dark(),
+    //   navigatorObservers: [
+    //     FirebaseAnalyticsObserver(analytics: analytics),
+    //   ],
+    //   initialRoute: '/',
+    //   routes: {
+    //     "/": (context) => HomePage(),
+    //     "/setting": (context) => SettingPage(),
+    //   },
+    // );
   }
 }
