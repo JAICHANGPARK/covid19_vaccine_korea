99f487f6720a8158d28fddf8d8ed1591d51224ca
Dreamwalker <aristojeff@gmail.com>
2021-03-13 14:51:03 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:fimber/fimber.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
