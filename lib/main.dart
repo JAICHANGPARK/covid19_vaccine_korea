f098e2c06d8fac0c93a80cc9b8700492039d99b2
Dreamwalker <aristojeff@gmail.com>
2021-03-13 22:40:52 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:fimber/fimber.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'src/ui/screen/main_app.dart';
 
 
+Future<void> initHive()async {
+  await Hive.initFlutter();
+
+}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  initHive();
   if(kDebugMode){
     Fimber.plantTree(DebugTree());
     Fimber.d('debug mode');
