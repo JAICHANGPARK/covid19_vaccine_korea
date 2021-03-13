ec3bca780e7cc9b74fc5ab504820198ec50d077e
Dreamwalker <aristojeff@gmail.com>
2021-03-14 00:36:24 +0900

    Update new_note_page.dart

--- lib/src/ui/screen/note/new_note_page.dart
+++ lib/src/ui/screen/note/new_note_page.dart
@@ -1,4 +1,10 @@
-///
+import 'package:covid_19_vaccine_korea/src/db/db.dart';
+import 'package:covid_19_vaccine_korea/src/db/object/note.dart';
+import 'package:fimber/fimber.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../main.dart';
+
 /// 기침
 // 인후통
 // 발열
@@ -9,4 +15,25 @@
 // 설사
 // 콧물
 // 복통
-///
+
+class NewNotePage extends StatefulWidget {
+  @override
+  _NewNotePageState createState() => _NewNotePageState();
+}
+
+class _NewNotePageState extends State<NewNotePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async{
+          int? index = await getIt.get<AppDBProvider>().addNote(Note()
+            ..title = "Test"
+            ..created = DateTime.now()
+            ..description = "test test");
+          Fimber.d(">>> index : $index");
+        },
+      ),
+    );
+  }
+}
