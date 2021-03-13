9adb0b871617c6cd223cd947ca2c6d669e2e4fb6
Dreamwalker <aristojeff@gmail.com>
2021-03-14 00:36:29 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,28 @@
+import 'package:covid_19_vaccine_korea/src/db/db.dart';
 import 'package:covid_19_vaccine_korea/src/db/object/note.dart';
 import 'package:fimber/fimber.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'src/ui/screen/main_app.dart';
 
+// This is our global ServiceLocator
+GetIt getIt = GetIt.instance;
 
-Future<void> initHive()async {
-  await Hive.initFlutter();
-  Hive.registerAdapter(NoteAdapter());
-}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  initHive();
-  if(kDebugMode){
+
+  if (kDebugMode) {
     Fimber.plantTree(DebugTree());
     Fimber.d('debug mode');
   }
+
+  getIt.registerSingleton<AppDBProvider>(AppDBProvider(), signalsReady: true);
+  getIt.get<AppDBProvider>().initHive();
+
   runApp(Covid19VaccineApp());
 }
