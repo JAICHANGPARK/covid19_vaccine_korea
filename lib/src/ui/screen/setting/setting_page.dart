9ba6de410d2de9f274e16f9f30ac369dd57f07c5
Dreamwalker <aristojeff@gmail.com>
2021-03-14 14:29:58 +0900

    Update setting_page.dart

--- lib/src/ui/screen/setting/setting_page.dart
+++ lib/src/ui/screen/setting/setting_page.dart
@@ -1,3 +1,5 @@
+import 'package:covid_19_vaccine_korea/main.dart';
+import 'package:covid_19_vaccine_korea/src/db/db.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
 
@@ -87,7 +89,46 @@ class _SettingPageState extends State<SettingPage> {
                   ),
                 ),
               ),
+              SizedBox(height: 16,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                child: Text("데이터"),
+              ),
+              PhysicalModel(
+                color: Colors.grey,
+                child: Container(
+                  color: Theme.of(context).cardColor,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        onTap: ()async{
+                          showDialog(context: context, builder: (context){
+                            return AlertDialog(
+                              title: Text("알림"),
+                              content: Text("모든 기록을 삭제할까요?"),
+                              actions: [
+                                ElevatedButton(onPressed: ()async{
+                                  await getIt.get<AppDBProvider>().clearDatabase();
+                                  Navigator.of(context).pop();
 
+                                }, child: Text("네"))
+                              ],
+                            );
+                          });
+
+                        },
+                        title: Text("기록 전체 삭제"),
+                        subtitle: Text("모든 기록을 삭제합니다."),
+
+                      ),
+                      // Divider(height: 8,),
+
+
+
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
