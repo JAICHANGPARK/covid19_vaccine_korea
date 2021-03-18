9586761e314b070171c2a1d83228b688e5efb8f7
Dreamwalker <aristojeff@gmail.com>
2021-03-18 20:21:00 +0900

    Update note_record_widget.dart

--- lib/src/ui/widgets/note_record_widget.dart
+++ lib/src/ui/widgets/note_record_widget.dart
@@ -3,6 +3,7 @@ import 'package:covid_19_vaccine_korea/src/db/object/note.dart';
 import 'package:fimber/fimber.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import '../../../main.dart';
 
@@ -21,7 +22,7 @@ class _NoteRecordWidgetState extends State<NoteRecordWidget> {
     // TODO: implement initState
     super.initState();
     Fimber.d(">>> initState");
-    Future.delayed(Duration(seconds: 2), () {
+    Future.delayed(Duration(seconds: 3), () {
       isLoading = true;
       setState(() {
         noteItems = getIt.get<AppDBProvider>().noteBox?.values.toList();
@@ -45,57 +46,76 @@ class _NoteRecordWidgetState extends State<NoteRecordWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return noteItems!.length > 0
-        ? RefreshIndicator(
-            onRefresh: refreshItemList,
-            child: ListView.builder(
-              itemCount: noteItems!.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("${noteItems![index].created.toString().split(".").first}"),
-                        Text("${noteItems![index].symptom ?? "증상 없음."}"),
-                        Text("${noteItems![index].description ?? "상세 기록 없음."}")
-                      ],
-                    ),
+    if(getIt.get<AppDBProvider>().noteBox != null)
+    return ValueListenableBuilder<Box<Note>>(
+      valueListenable: getIt.get<AppDBProvider>().noteBox!.listenable(),
+      builder: (context, box, widget) {
+        noteItems = box.values.toList();
+        if(noteItems!.length > 0){
+          return ListView.builder(
+            itemCount: noteItems!.length,
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("일자: ${noteItems![index].created.toString().split(".").first}"),
+                      Text("증상: ${noteItems![index].symptom ?? "증상 없음."}"),
+                      Text("기록"),
+                      Text("${noteItems![index].description ?? "상세 기록 없음."}")
+                    ],
                   ),
-                );
-                return Text("${noteItems![index].created.toString()}: ${noteItems![index].symptom}");
-              },
-            ),
-          )
-        : Column(
+                ),
+              );
+            },
+          );
+        }else{
+          return Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               isLoading
                   ? Column(
-                      children: [
-                        Image.asset(
-                          "assets/img/business-3d.png",
-                          width: MediaQuery.of(context).size.width / 1.5,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 16),
-                          child: Text("일지 기록이 없습니다."),
-                        ),
-                        ElevatedButton(
-                            onPressed: () {
-                              refreshItemList();
-                            },
-                            child: Text("새로고침"))
-                      ],
-                    )
+                children: [
+                  Image.asset(
+                    "assets/img/business-3d.png",
+                    width: MediaQuery.of(context).size.width / 1.5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: Text("일지 기록이 없습니다."),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        refreshItemList();
+                      },
+                      child: Text("새로고침"))
+                ],
+              )
                   : Center(
-                      child: Column(
-                      children: [CircularProgressIndicator(), Text("불러오는 중.")],
-                    )),
+                  child: Column(
+                    children: [CircularProgressIndicator(), Text("불러오는 중.")],
+                  )),
             ],
           );
+        }
+
+      },
+    );
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircularProgressIndicator(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("불러오는중"),
+          )
+        ],
+      ),
+    );
   }
 }
