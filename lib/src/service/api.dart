d715de840099e9ab6b67759fc5eb6b13217c6e68
Dreamwalker <aristojeff@gmail.com>
2021-03-27 15:55:37 +0900

    Update api.dart

--- lib/src/service/api.dart
+++ lib/src/service/api.dart
@@ -5,6 +5,7 @@ import 'package:covid_19_vaccine_korea/src/service/api_key.dart';
 import 'package:dio/dio.dart';
 import 'package:fimber/fimber.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 Future<VaccineCenter?> fetchVaccineCenter() async {
   try {
@@ -44,4 +45,24 @@ Future fetchVaccineCenterOld() async {
   } else {
     print('Request failed with status: ${response.statusCode}.');
   }
+}
+
+Future fetchVaccineCount(int start, int endPage, String? baseDate) async {
+  if(baseDate == null){
+    baseDate =  DateFormat("yyyy-MM-dd 00:00:00").format(DateTime.now());
+  }
+  var response = await http
+      .get("https://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=${start}&perPage=${endPage}"
+      "&cond%5BbaseDate%3A%3AEQ%5D=$baseDate&serviceKey=${ApiKey.API_KEY_2}");
+  print(response.body);
+  if (response.statusCode == 200) {}
+}
+
+Future fetchVaccineCountBySido(int start, int endPage, String sido) async {
+  
+  var response = await http
+      .get("https://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=${start}&perPage=${endPage}"
+      "&cond%5Bsido%3A%3AEQ%5D=$sido&serviceKey=${ApiKey.API_KEY_2}");
+  print(response.body);
+  if (response.statusCode == 200) {}
 }
