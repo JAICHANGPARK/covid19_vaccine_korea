57dc7408b98f1746e2a9523cd77aad891a3356ba
Dreamwalker <aristojeff@gmail.com>
2021-04-09 20:42:29 +0900

    Update vaccine_count_detail_page.dart

--- lib/src/ui/screen/vaccine_count_detail_page.dart
+++ lib/src/ui/screen/vaccine_count_detail_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 class VaccineCountDetailPage extends StatefulWidget {
   final String sidoName;
 
-
   VaccineCountDetailPage({required this.sidoName});
 
   @override
@@ -18,8 +17,31 @@ class _VaccineCountDetailPageState extends State<VaccineCountDetailPage> {
     super.initState();
     fetchVaccineCountBySido(1, 100, widget.sidoName);
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: Icon(Icons.arrow_back)),
+                    Text("${widget.sidoName} 상세 정보", style: TextStyle(
+                      fontSize: 18
+                    ),)
+                  ],
+                )),
+            Expanded(flex: 15, child: Placeholder())
+          ],
+        ),
+      ),
+    );
   }
 }
