5dfd2d4c67ce5231ced7510439ecec46d8f85fab
Dreamwalker <aristojeff@gmail.com>
2021-04-10 13:24:50 +0900

    Update vaccine_count_detail_page.dart

--- lib/src/ui/screen/vaccine_count_detail_page.dart
+++ lib/src/ui/screen/vaccine_count_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:covid_19_vaccine_korea/src/model/vaccine_count.dart';
 import 'package:covid_19_vaccine_korea/src/service/api.dart';
+import 'package:covid_19_vaccine_korea/src/ui/widgets/detail_chart_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -167,7 +168,27 @@ class _VaccineCountDetailPageState extends State<VaccineCountDetailPage> {
                       child: SizedBox(
                           height: 200,
                           child: Card(
-                            elevation: 4,
+                            elevation: 8,
+                            child: FutureBuilder<VaccineCount?>(
+                              future: fetchVaccineCountBySido(1, 100, widget.sidoName),
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  if (snapshot.data?.data?.length == 0) return Text("데이터 없음");
+                                  if (snapshot.data != null) {
+
+
+                                    return DetailChartWidget(vaccineCount: snapshot.data,);
+                                  }
+                                }
+                                return Center(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [CircularProgressIndicator(), Text("불러오는 중")],
+                                  ),
+                                );
+                              },
+                            ),
                           )),
                       left: 16,
                       right: 16,
