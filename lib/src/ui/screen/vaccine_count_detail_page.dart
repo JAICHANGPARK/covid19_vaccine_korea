a4e408e32da28ce0d24711d5c2549ee7708a5b5e
Dreamwalker <aristojeff@gmail.com>
2021-04-10 13:06:57 +0900

    Update vaccine_count_detail_page.dart

--- lib/src/ui/screen/vaccine_count_detail_page.dart
+++ lib/src/ui/screen/vaccine_count_detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:covid_19_vaccine_korea/src/model/vaccine_count.dart';
 import 'package:covid_19_vaccine_korea/src/service/api.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class VaccineCountDetailPage extends StatefulWidget {
   final String sidoName;
@@ -12,6 +13,9 @@ class VaccineCountDetailPage extends StatefulWidget {
 }
 
 class _VaccineCountDetailPageState extends State<VaccineCountDetailPage> {
+
+  final formatCurrency = NumberFormat.simpleCurrency(locale: "ko_KR", name: "", decimalDigits: 0);
+
   @override
   void initState() {
     // TODO: implement initState
@@ -44,29 +48,77 @@ class _VaccineCountDetailPageState extends State<VaccineCountDetailPage> {
                 flex: 20,
                 child: Stack(
                   children: [
-
                     Positioned(
                       child: SizedBox(child: Card(
-                        child: FutureBuilder<VaccineCount?>(
-                          future:fetchVaccineCountBySido(1, 100, widget.sidoName),
-                          builder: (context, snapshot){
-                            if(snapshot.hasData){
-                              if(snapshot.data?.data?.length == 0) return Text("데이터 없음");
-                              if(snapshot.data != null){
-                                
-                              }
-                            }
-                            return Center(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.center,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 64),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Expanded(child: Text("날짜")),
+                                    Expanded(child: Text("1차접종")),
+                                    Expanded(child: Text("2차접종")),
+                                  ],
+                                ),
+                              ),
+                              Divider(color: Colors.grey,),
+                              Row(
                                 children: [
-                                  CircularProgressIndicator(),
-                                  Text("불러오는 중")
+                                  Expanded(child: Text("")),
+                                  Expanded(child: Text("당일실적")),
+                                  Expanded(child: Text("당일누적")),
+                                  Expanded(child: Text("당일실적")),
+                                  Expanded(child: Text("당일누적")),
                                 ],
                               ),
-                            );
-                          },
+                              Expanded(
+                                child: FutureBuilder<VaccineCount?>(
+                                  future:fetchVaccineCountBySido(1, 100, widget.sidoName),
+                                  builder: (context, snapshot){
+                                    if(snapshot.hasData){
+                                      if(snapshot.data?.data?.length == 0) return Text("데이터 없음");
+                                      if(snapshot.data != null){
+                                        List<Data?> result = snapshot.data!.data!;
+                                        result = result.reversed.toList();
+                                        return ListView.separated(itemBuilder: (context, index){
+                                          Data? d = result[index];
+
+                                          return  Row(
+                                            children: [
+                                              Expanded(child: Text("${d?.baseDate?.split(" ").first}")),
+                                              Expanded(child: Text("${formatCurrency.format(d?.firstCnt)} 명")),
+                                              Expanded(child: Text("${formatCurrency.format(d?.totalFirstCnt)} 명")),
+                                              Expanded(child: Text("${formatCurrency.format(d?.secondCnt)} 명")),
+                                              Expanded(child: Text("${formatCurrency.format(d?.totalSecondCnt)} 명")),
+                                            ],
+                                          );
+                                        },
+                                        itemCount: result.length,
+                                          separatorBuilder: (context, index){
+                                          return Divider(color: Colors.grey,);
+                                          },
+                                        );
+                                      }
+                                    }
+                                    return Center(
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          CircularProgressIndicator(),
+                                          Text("불러오는 중")
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
 
                       )),
